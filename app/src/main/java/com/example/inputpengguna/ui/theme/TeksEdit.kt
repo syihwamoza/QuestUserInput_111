abfdc5b3fe058d95389cbb0282daf0d249f6bb5f
syihwamoza <mozakastella26@gmail.com>
2025-10-30 15:32:37 +0700

    Menambahkan variabel 'jenis' dengan remember untuk menyimpan pilihan gender dari UI

--- app/src/main/java/com/example/inputpengguna/ui/theme/TeksEdit.kt
+++ app/src/main/java/com/example/inputpengguna/ui/theme/TeksEdit.kt
@@ -40,4 +40,5 @@ fun FormDataDiri (modifier: Modifier
     //variabel-variabel untuk menyimpan data yang diperoleh dari komponen UI
     var nama by remember { mutableStateOf("") }
     var alamat by remember { mutableStateOf("") }
+    var jenis by remember { mutableStateOf("") }
 }
