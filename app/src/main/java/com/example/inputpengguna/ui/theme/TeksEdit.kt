4c1f0158b98b45351b989d68f8ed458192ffd47b
syihwamoza <mozakastella26@gmail.com>
2025-10-30 15:33:50 +0700

    Menambahkan Column dengan padding dan alignment, serta mulai menambahkan OutlinedTextField

--- app/src/main/java/com/example/inputpengguna/ui/theme/TeksEdit.kt
+++ app/src/main/java/com/example/inputpengguna/ui/theme/TeksEdit.kt
@@ -44,4 +44,9 @@ fun FormDataDiri (modifier: Modifier
 
     val gender: List<String> = listOf("Laki-laki", "Perempuan")
 
-}
+    Column(modifier = Modifier.padding(top = 50.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        OutlinedTextField(
+
+    }
