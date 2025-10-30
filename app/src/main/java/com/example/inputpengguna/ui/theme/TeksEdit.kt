8ebc6b4c24ef20f863433234c23a79e5f396efed
syihwamoza <mozakastella26@gmail.com>
2025-10-30 15:33:16 +0700

    Menambahkan daftar gender dengan pilihan "Laki-laki" dan "Perempuan"

--- app/src/main/java/com/example/inputpengguna/ui/theme/TeksEdit.kt
+++ app/src/main/java/com/example/inputpengguna/ui/theme/TeksEdit.kt
@@ -41,4 +41,7 @@ fun FormDataDiri (modifier: Modifier
     var nama by remember { mutableStateOf("") }
     var alamat by remember { mutableStateOf("") }
     var jenis by remember { mutableStateOf("") }
+
+    val gender: List<String> = listOf("Laki-laki", "Perempuan")
+
 }
