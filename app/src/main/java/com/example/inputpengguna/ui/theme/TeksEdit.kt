1d629371127b147de6926bd806fd25c250a00ad1
syihwamoza <mozakastella26@gmail.com>
2025-10-30 15:31:04 +0700

    menambah variabel "nama" dengan remember untuk menyimpan data dari UI

--- app/src/main/java/com/example/inputpengguna/ui/theme/TeksEdit.kt
+++ app/src/main/java/com/example/inputpengguna/ui/theme/TeksEdit.kt
@@ -33,7 +33,11 @@ import com.example.inputpengguna.R
 fun FormDataDiri (modifier: Modifier
 ){
     //variabel-variabel untuk mengingat nilai masukan dari keyboard
-    var textNama by remember { mutableStateOf("")
-        var textAlamat by remember { mutableStateOf("") }
-        var textJK by remember { mutableStateOf("") }
-    }
+    var textNama by remember { mutableStateOf("") }
+    var textAlamat by remember { mutableStateOf("") }
+    var textJK by remember { mutableStateOf("") }
+
+    //variabel-variabel untuk menyimpan data yang diperoleh dari komponen UI
+    var nama by remember { mutableStateOf("") }
+
+}
